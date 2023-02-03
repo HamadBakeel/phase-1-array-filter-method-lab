057ed5e8c91633c327f34d64d8509a61fd48be2a
Hamad Bakeel <90466313+HamadBakeel@users.noreply.github.com>
2023-02-03 22:39:08 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching(drivers, name){
+  return drivers.filter(driver=> driver.toUpperCase() === name.toUpperCase())
+}
+
+function  fuzzyMatch(drivers, starter){
+  return drivers.filter(driver=> driver.startsWith(starter))
+}
+
+function matchName(drivers, name){
+  return drivers.filter(driver=> driver.name === name)
+}
